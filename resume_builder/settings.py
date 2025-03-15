293768b28374843d9311e1b8e7baa0b6248482ca
Parmeet Kumar <parmeetkumarg90@gmail.com>
2025-03-15 20:32:24 +0530

    Update settings.py

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -127,14 +127,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR,'static'
+    os.path.join(BASE_DIR,'static')
 ] 
-#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')  #vercel
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')  #vercel
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
