a528e91acd97541eef7470b040954fb36bea21be
Parmeet Kumar <parmeetkumarg90@gmail.com>
2025-03-15 20:19:21 +0530

    completed

---
+++ resume_builder/views.py
@@ -0,0 +1,195 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+from userdata.models import user_data
+from datetime import date
+import ast  # used for making models returning string into list by removing literals
+
+def homepage(request):
+    return render(request,'index.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        html_username = request.POST['html_username']
+        html_password = request.POST['html_password']
+        if (html_username == '' or html_password == ''):
+            return render(request,'login.html',{ 'output' : 'Wrong UserName or Password' })
+        else:
+            # Check if the user exists with the exact username (case-sensitive)
+            try:
+                user = User.objects.get(username=html_username)
+            except User.DoesNotExist:
+                user = None
+
+            # If user exists, authenticate with the password
+            if user is not None and user.check_password(html_password):
+                login(request, user)
+                request.session['pass'] = html_password
+                return redirect('/resumebuild/')
+            else:
+                return render(request,'login.html',{ 'output' : "Don't have an account"})
+    return render(request,'login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        html_username = request.POST['html_username']
+        html_password = request.POST['html_password']
+        conf_password = request.POST['html_confpassword']
+        if (html_password != conf_password) or (html_username == '' or html_password == '' or conf_password == ''):
+            return render(request,'signup.html',{ 'output':'Wrong UserName or Password' })
+        else:
+            isuser = authenticate(request,username = html_username, password = html_password)
+            if isuser is not None:
+                return render(request,'signup.html',{ 'output':'You have already an account' })
+            else:
+                user = User.objects.create_user(username=html_username,password=html_password)
+                user.save()
+                return render(request,'signup.html',{ 'output' : 'Account created Now Go to LogIn page'})
+    return render(request,'signup.html')
+
+def resume(request):        
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            obj = user_data.objects.filter(username=str(request.user)+str(request.session.get('pass',''))).first()
+            if not obj:
+                data = user_data(
+                    username = str(request.user)+str(request.session.get('pass','')),
+
+                    intro_firstname = request.POST.get('firstname',''),
+                    intro_middlename = request.POST.get('middlename',''),
+                    intro_lastname = request.POST.get('lastname',''),
+                    intro_image = request.FILES.get('image'),
+                    intro_designation = request.POST.get('designation',''),
+                    intro_address = request.POST.get('address',''),
+                    intro_email = request.POST.get('email',''),
+                    intro_phone = request.POST.get('phoneno',''),
+                    intro_summary = request.POST.get('summary',''),
+
+                    achi_title = request.POST.getlist('achieve_title',''),
+                    achi_description = request.POST.getlist('achieve_description',''),
+
+                    exp_title = request.POST.getlist('exp_title',''),
+                    exp_company = request.POST.getlist('exp_organization',''),
+                    exp_location = request.POST.getlist('exp_location',''),
+                    exp_start_date = request.POST.getlist('exp_start_date', ''),
+                    exp_end_date = request.POST.getlist('exp_end_date', ''),
+                    exp_description = request.POST.getlist('exp_description',''),
+
+                    edu_school = request.POST.getlist('edu_school',''),
+                    edu_degree = request.POST.getlist('edu_degree',''),
+                    edu_city = request.POST.getlist('edu_city',''),
+                    edu_start_date = request.POST.getlist('edu_start_date',''),
+                    edu_end_date = request.POST.getlist('edu_graduation_date',''),
+                    edu_description = request.POST.getlist('edu_description',''),
+
+                    proj_name = request.POST.getlist('proj_title',''),
+                    proj_link = request.POST.getlist('proj_link',''),
+                    proj_description = request.POST.getlist('proj_description',''),
+
+                    skills = request.POST.getlist('skill','')
+                )
+                data.save()
+            else:
+                # If a record exists, update it
+                if request.POST.get('firstname'):
+                    obj.intro_firstname = request.POST.get('firstname', '')
+                if request.POST.get('middlename'):
+                    obj.intro_middlename = request.POST.get('middlename', '')
+                if request.POST.get('lastname'):
+                    obj.intro_lastname = request.POST.get('lastname', '')
+                if request.POST.get('image'):
+                    obj.intro_image = request.FILES.get('image')
+                if request.POST.get('designation'):
+                    obj.intro_designation = request.POST.get('designation', '')
+                if request.POST.get('address'):
+                    obj.intro_address = request.POST.get('address', '')
+                if request.POST.get('email'):
+                    obj.intro_email = request.POST.get('email', '')
+                if request.POST.get('phoneno'):
+                    obj.intro_phone = request.POST.get('phoneno', '')
+                if request.POST.get('summary'):
+                    obj.intro_summary = request.POST.get('summary', '')
+
+                if request.POST.getlist('achieve_title'):
+                    obj.achi_title = request.POST.getlist('achieve_title', '')
+                if request.POST.getlist('achieve_description'):
+                    obj.achi_description = request.POST.getlist('achieve_description', '')
+
+                if request.POST.getlist('exp_title'):
+                    obj.exp_title = request.POST.getlist('exp_title', '')
+                if request.POST.getlist('exp_organization'):
+                    obj.exp_company = request.POST.getlist('exp_organization', '')
+                if request.POST.getlist('exp_location'):
+                    obj.exp_location = request.POST.getlist('exp_location', '')
+                if request.POST.getlist('exp_start_date'):
+                    obj.exp_start_date = request.POST.getlist('exp_start_date', date.today())
+                if request.POST.getlist('exp_end_date'):
+                    obj.exp_end_date = request.POST.getlist('exp_end_date', date.today())
+                if request.POST.getlist('exp_description'):
+                    obj.exp_description = request.POST.getlist('exp_description', '')
+
+                if request.POST.getlist('edu_school'):
+                    obj.edu_school = request.POST.getlist('edu_school', '')
+                if request.POST.getlist('edu_degree'):
+                    obj.edu_degree = request.POST.getlist('edu_degree', '')
+                if request.POST.getlist('edu_city'):
+                    obj.edu_city = request.POST.getlist('edu_city', '')
+                if request.POST.getlist('edu_start_date'):
+                    obj.edu_start_date = request.POST.getlist('edu_start_date', date.today())
+                if request.POST.getlist('edu_graduation_date'):
+                    obj.edu_end_date = request.POST.getlist('edu_graduation_date', date.today())
+                if request.POST.getlist('edu_description'):
+                    obj.edu_description = request.POST.getlist('edu_description', '')
+
+                if request.POST.getlist('proj_title'):
+                    obj.proj_name = request.POST.getlist('proj_title', '')
+                if request.POST.getlist('proj_link'):
+                    obj.proj_link = request.POST.getlist('proj_link', '')
+                if request.POST.getlist('proj_description'):
+                    obj.proj_description = request.POST.getlist('proj_description', '')
+                    
+                if request.POST.getlist('skill'):
+                    obj.skills = request.POST.getlist('skill', '')
+                obj.save()
+            return render(request,'resume.html')
+        
+        obj = user_data.objects.filter(username=str(request.user)+str(request.session.get('pass',''))).first()
+        if obj:
+            obj.achi_title = eval(obj.achi_title)
+            obj.achi_description = eval(obj.achi_description)
+
+            obj.exp_title = eval(obj.exp_title)
+            obj.exp_company = eval(obj.exp_company)
+            obj.exp_location = eval(obj.exp_location)
+            obj.exp_start_date = obj.exp_start_date
+            obj.exp_end_date = obj.exp_end_date
+            obj.exp_description = eval(obj.exp_description)
+
+            obj.edu_school = eval(obj.edu_school)
+            obj.edu_degree = eval(obj.edu_degree)
+            obj.edu_city = eval(obj.edu_city)
+            obj.edu_start_date = obj.edu_start_date
+            obj.edu_end_date = obj.edu_end_date
+            obj.edu_description = eval(obj.edu_description)
+
+            obj.proj_name = eval(obj.proj_name)
+            obj.proj_link = eval(obj.proj_link)
+            obj.proj_description = eval(obj.proj_description)
+
+            obj.skills = eval(obj.skills)
+            return render(request,'resume.html',
+            {'output': obj,
+            'achievements':zip(obj.achi_title, obj.achi_description),
+            'experiences':zip(obj.exp_title,obj.exp_company,obj.exp_location,obj.exp_start_date,obj.exp_end_date,obj.exp_description),
+            'educations':zip(obj.edu_school,obj.edu_degree,obj.edu_city,obj.edu_start_date,obj.edu_end_date,obj.edu_description),
+            'projects':zip(obj.proj_name,obj.proj_link,obj.proj_description),
+            'skills':obj.skills
+            })
+        return render(request,'resume.html')
+    else:
+        return redirect('/signup/')
+    
+def signout(request):
+    logout(request)
+    return redirect('/login/')
---
+++ userdata/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+# Create your models here.
+class user_data(models.Model):
+    username = models.CharField(max_length=15,verbose_name="UserName",default = '')
+
+    intro_firstname = models.CharField(max_length=50,verbose_name="FirstName",default = '')
+    intro_middlename = models.CharField(max_length=50,verbose_name="MiddleName",default = '')
+    intro_lastname = models.CharField(max_length=50,verbose_name="LastName",default = '')
+    intro_image = models.FileField(upload_to='',verbose_name="User Image",null = True,default=None,max_length=250)
+    intro_designation = models.TextField(verbose_name="Designation",default = '')
+    intro_address = models.TextField(verbose_name="User Address",default = '')
+    intro_email = models.EmailField(verbose_name="User Email",default = '')
+    intro_phone = models.CharField(max_length=50,verbose_name="User Mobile Number",default = '')
+    intro_summary = models.TextField(verbose_name="User Summary",default = '')
+
+    achi_title = models.CharField(max_length=50,verbose_name="Achievement Title",default = list)
+    achi_description = models.TextField(verbose_name="Achievement Description",default = list)
+
+    exp_title = models.CharField(max_length=50,verbose_name="Experience Title",default = list)
+    exp_company = models.CharField(max_length=50,verbose_name="Experience Company",default = list)
+    exp_location = models.CharField(max_length=50,verbose_name="Experience Location",default = list)
+    exp_start_date = models.JSONField(verbose_name="Experience Start Date",null=True,blank=True)
+    exp_end_date = models.JSONField(verbose_name="Experience End Date",null=True,blank=True)
+    exp_description = models.TextField(verbose_name="Experience Description",default = list)
+
+    edu_school = models.CharField(max_length=50,verbose_name="Education School",default = list)
+    edu_degree = models.CharField(max_length=50,verbose_name="Education Degree",default = list)
+    edu_city = models.CharField(max_length=50,verbose_name="Education City",default = list)
+    edu_start_date = models.JSONField(verbose_name="Education Start Date",null=True,blank=True)
+    edu_end_date = models.JSONField(verbose_name="Education End Date",null=True,blank=True)
+    edu_description = models.TextField(verbose_name="Education Description",default = list)
+
+    proj_name = models.CharField(max_length=50,verbose_name="Project Name",default = list)
+    proj_link = models.CharField(max_length=50,verbose_name="Project Link",default = list)
+    proj_description = models.TextField(verbose_name="Project Description",default = list)
+
+    skills = models.TextField(verbose_name="your Skills",default = list)
